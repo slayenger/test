9812ebad2b2f2271b50d441eecdfd41dbf5e4a8c
slayenger <sinyavskiym@mail.ru>
2020-10-25 00:31:20 +0300

    add a programm

---
+++ first programm.pas
@@ -0,0 +1,6 @@
+﻿var a : string; 
+begin 
+  writeln('Как тебя зовут?');
+  read (a) ; 
+  writeln ('Привет, ', a);
+end.
--- whatsurname.pas
+++
@@ -1,9 +0,0 @@
-﻿var a:string; 
-begin 
-writeln('Как тебя зовут?');
-readln(a);
-<<<<<<< HEAD
-add the string;
-writeln('a new commit');
-writeln('Привет, ',a);
-end.
